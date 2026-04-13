8c14762dad011be49712c61327bb1d7d354cd99a
Justin King <justin.king6@msubillings.edu>
2026-04-13 13:10:27 -0600

    add room to assignment resource

--- app/Http/Resources/AssignmentResource.php
+++ app/Http/Resources/AssignmentResource.php
@@ -20,6 +20,7 @@ class AssignmentResource extends JsonResource
             'attributes' => [
                 'course_name' => $this->section->course->name,
                 'days' => $this->timeslot->days,
+                'room_name' => $this->room->name,
                 'start_time' => $this->timeslot->start_time,
                 'duration' => $this->timeslot->duration,
             ]
