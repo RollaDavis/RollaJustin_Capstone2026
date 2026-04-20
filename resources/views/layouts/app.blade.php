6613d55ae3280d4c1523e6711b8d43f28e34798a
Rolla Davis <rolla.davislowery@msubillings.edu>
2026-04-20 16:44:04 -0600

    ;

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 
     <!-- Scripts -->
     {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css']) --}}
-    <link href="/build/assets/app-C8pPhopy.css" rel="stylesheet">
+    <link href="/css/light-mode.css" rel="stylesheet">
     <script src="/build/assets/app-Dl6olWsf.js"></script>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
