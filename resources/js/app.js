b24e9c133494de4cd67205dc28592873f0dc1352
Rolla Davis <rolla.davislowery@msubillings.edu>
2026-04-22 13:43:14 -0600

    ;

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,4 @@
 import './setup-csrf.js';
-import './ajax-forms.js';
 import { Calendar } from '@fullcalendar/core';
 import interactionPlugin from '@fullcalendar/interaction';
 import dayGridPlugin from '@fullcalendar/daygrid';
